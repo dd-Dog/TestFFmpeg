4bd78267c9fe1f39d650ad78f645f81335024f71
bianjb <bianjb@flyscale.cn>
2019-11-19 20:55:16 +0800

    Initial Commit

---
+++ app/src/main/java/com/flyscale/testforffmpeg/MainActivity.java
@@ -0,0 +1,27 @@
+package com.flyscale.testforffmpeg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(NativeAPI.stringFromJNI());
+        Log.d(getClass().getSimpleName(), NativeAPI.avformatinfo());
+    }
+
+}
---
+++ app/src/main/java/com/flyscale/testforffmpeg/NativeAPI.java
@@ -0,0 +1,15 @@
+package com.flyscale.testforffmpeg;
+
+public class NativeAPI {
+
+    public static native String stringFromJNI();
+
+    public static native String urlprotocolinfo();
+
+    public static native String avformatinfo();
+
+    public static native String avcodecinfo();
+
+    public static native String avfilterinfo();
+
+}
